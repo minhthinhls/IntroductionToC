8a849d49a81fe586b2bdec528cee8ca54c1b9152
Huỳnh Thịnh <minhthinhlss@gmail.com>
2019-08-02 02:22:13 +0700

    Update Lab Assignments, Modify Lab05 - Fundamental Programming

--- src/Lab05_FP.c
+++ src/Lab05_FP.c
@@ -2,9 +2,45 @@
 #include <string.h>	// strlen(), strcpy()
 #include <stdlib.h>	// malloc(), calloc()
 
+void swapStrings(char** str1, char** str2){
+    char *temp = *str1;
+    *str1 = *str2;
+    *str2 = temp;
+}
+
+char* getArg(int strlength) { // Get an argument / word !
+	char *arg = (char*) calloc(strlength, sizeof(char));
+	return arg;
+}
+
+char** getArgv(int vectorlength, int strlength) { // Get an argument vector / list-of-words !
+	int i;
+	char **argv = (char**) calloc(vectorlength, sizeof(char*));
+	for (i = 0; i < strlength; i++) { // Dynamic-continuous allocation for each word in the array of words !
+		argv[i] = (char*) calloc(strlength, sizeof(char));
+	}
+	return argv;
+}
+
+char** split(char *s, char *delimiter) {
+	int i, index = 1, strlength = strlen(s) + 1;
+	char *buffer = getArg(10); // Buffer to convert int to String !
+	char *str = getArg(strlength); // Initialized $<str> to cut String using strtok() with delimiter.
+	char *word = getArg(strlength);
+	char **words = getArgv(strlength, strlength);
+	char *p = strtok(strcpy(str, s), delimiter); // Copy $<s> to $<str> and run the first delimiting program !
+	while (p != NULL) { // Repeat the String-cutting process !
+		strcpy(words[index++], p); // Assign the trimmed word $<p> to array of words !
+		p = strtok(NULL, delimiter);
+	}
+	words[index] = NULL; // Mark that the list-of-words ends at index (values) !
+	words[0] = itoa(index - 1, buffer, 10); // List-of-words only has N = index - 1 (values) ; Stored at words[0] !
+	return words; // $<words[0]> == Numbers of words contained in this vector ! $<words>[1...index - 1] == the splitted words !
+}
+
 void ex01(char *s, char *delimiter) {
 	printf("\n-----EXERCISE 1-----\n");
-	char *p, *str = (char*) calloc(strlen(s) + 1, sizeof(char));
+	char *p, *str = getArg(strlen(s) + 1);
 	printf("The String is: <%s>\n", strcpy(str, s));
 	do {
 		p = strtok(str, delimiter); // Return the trimmed String <$p> before the $delimiter
@@ -15,12 +51,12 @@ void ex01(char *s, char *delimiter) {
 		}
 	} while(p != NULL);
 	printf("-> String after running: <%s>\n", str);
-	printf("-> String as input: <%s>", s);
+	printf("-> String as input: <%s>\n", s);
 }
 
 void ex02(char *s1, char *s2, char *s3) {
 	printf("\n-----EXERCISE 2-----\n");
-	char *input = (char*) calloc(strlen(s1) + strlen(s2) + 1, sizeof(char));
+	char *input = getArg(strlen(s1) + strlen(s2) + 1);
 	strcpy(input, s1);
 	printf("$input before concatenation: %s\n", input);
 	strcat(input, s2);
@@ -31,21 +67,21 @@ void ex02(char *s1, char *s2, char *s3) {
 			k++;
 		}
 	}
-	printf("\nThe result <=> The times appear of [aeiou] in String: %d", k); 
+	printf("\nThe result <=> The times appear of [aeiou] in String: %d\n", k); 
 }
 
 int ex03(int maxLength) {
 	printf("\n-----EXERCISE 3-----\n");
-	char *str = (char*) calloc(maxLength, sizeof(char));
+	char *str = getArg(maxLength);
 	printf("Input the String: ");
 	int num = strlen(gets(str));
-	printf("-> Number of characters in String = %d", num);
+	printf("-> Number of characters in String = %d\n", num);
 	return num;
 }
 
 int ex04(char *s) {
 	printf("\n-----EXERCISE 4-----\n");
-	char *str = (char*) calloc(strlen(s) + 1, sizeof(char));
+	char *str = getArg(strlen(s) + 1);
 	printf("The String is: <%s>\n", strcpy(str, s));
 	int numSpace = 0, i;
 	for (i = 0; i < strlen(str); i++) {
@@ -53,13 +89,13 @@ int ex04(char *s) {
 			numSpace++;
 		}
 	}
-	printf("-> Num of spaces in String = %d", numSpace);
+	printf("-> Num of spaces in String = %d\n", numSpace);
 	return numSpace;
 }
 
 char* ex05(char *s) {
 	printf("\n-----EXERCISE 5-----\n");
-	char *str = (char*) calloc(strlen(s) + 1, sizeof(char));
+	char *str = getArg(strlen(s) + 1);
 	printf("The String is: <%s>\n", strcpy(str, s));
 	int i = 0, j = 0;
 	while (str[i]) {
@@ -69,21 +105,21 @@ char* ex05(char *s) {
 		i++;
 	}
 	str[j] = '\0';
-	printf("-> String after remove spaces = <%s>", str);
+	printf("-> String after remove spaces = <%s>\n", str);
 	return str;
 }
 
 char* ex06(char *s1, char *s2) {
 	printf("\n-----EXERCISE 6-----\n");
-	char *str = (char*) calloc(strlen(s1) + strlen(s2) + 1, sizeof(char));
+	char *str = getArg(strlen(s1) + strlen(s2) + 1);
 	printf("String 1 = %s\nString 2 = %s\n", strcpy(str, s1), s2);
-	printf("-> String concatenated = <%s>", strcat(str, s2));
+	printf("-> String concatenated = <%s>\n", strcat(str, s2));
 	return str;
 }
 
 char* ex07(char *s) {
 	printf("\n-----EXERCISE 7-----\n");
-	char *str = (char*) calloc(strlen(s) + 1, sizeof(char));
+	char *str = getArg(strlen(s) + 1);
 	printf("The String is: <%s>\n", strcpy(str, s));
 	int i;
 	for (i = 0; i < strlen(str); i++) {
@@ -91,13 +127,13 @@ char* ex07(char *s) {
 			str[i] = str[i] + 32;
 		}
 	}
-	printf("-> String after lowercasing = <%s>", str);
+	printf("-> String after lowercasing = <%s>\n", str);
 	return str;
 }
 
 char* ex08(char *s) {
 	printf("\n-----EXERCISE 8-----\n");
-	char *str = (char*) calloc(strlen(s) + 1, sizeof(char));
+	char *str = getArg(strlen(s) + 1);
 	printf("The String is: <%s>\n", strcpy(str, s));
 	int i;
 	for (i = 0; i < strlen(str); i++) {
@@ -105,13 +141,13 @@ char* ex08(char *s) {
 			str[i] = str[i] - 32;
 		}
 	}
-	printf("-> String after uppercasing = <%s>", str);
+	printf("-> String after uppercasing = <%s>\n", str);
 	return str;
 }
 
 char* ex09(char *s) {
 	printf("\n-----EXERCISE 9-----\n");
-	char *str = (char*) calloc(strlen(s) + 1, sizeof(char));
+	char *str = getArg(strlen(s) + 1);
 	printf("The String is: <%s>\n", strcpy(str, s));
 	int i, toUpper = 1; // True
 	for (i = 0; i < strlen(str); i++) {
@@ -130,13 +166,13 @@ char* ex09(char *s) {
 			}
 		}
 	}
-	printf("-> String after title uppering = <%s>", str);
+	printf("-> String after title uppering = <%s>\n", str);
 	return str;
 }
 
 char* ex10(char *str) {
 	printf("\n-----EXERCISE 10-----\n");
-	char *toConvert = (char*) calloc(strlen(str) + 1, sizeof(char));
+	char *toConvert = getArg(strlen(str) + 1);
 	toConvert[0] = "\0";
 	printf("The String is: <%s>\n", str);
 	int i, j = 0;
@@ -163,13 +199,13 @@ char* ex10(char *str) {
 		}
 		isCharLower = !isCharLower; // Announce that the next char must be converted UPPER/lower !
 	}
-	printf("-> Interleaved Upper-Lower case of the String = <%s>", toConvert);
+	printf("-> Interleaved Upper-Lower case of the String = <%s>\n", toConvert);
 	return toConvert;
 }
 
 char* ex11(char *s) {
 	printf("\n-----EXERCISE 11-----\n");
-	char temp, *str = (char*) calloc(strlen(s) + 1, sizeof(char));
+	char temp, *str = getArg(strlen(s) + 1);
 	printf("The String is: <%s>\n", strcpy(str, s));
 	int i = 0, j = strlen(str) - 1;
 	while (i < j) {
@@ -177,14 +213,14 @@ char* ex11(char *s) {
 		str[i++] = str[j];
 		str[j--] = temp;
 	}
-	printf("-> Reverse of the String = <%s>", str);
+	printf("-> Reverse of the String = <%s>\n", str);
 	return str;
 }
 
 char* ex12(char *str, char c) {
 	printf("\n-----EXERCISE 12-----\n");
-	char *buffer = (char*) calloc(10, sizeof(char));
-	char *position = (char*) calloc(strlen(str) * 3, sizeof(char));
+	char *buffer = getArg(10);
+	char *position = getArg(strlen(str) * 3);
 	printf("The String is: <%s>\n", str);
 	int i;
 	for (i = 0; i < strlen(str); i++) {
@@ -193,7 +229,7 @@ char* ex12(char *str, char c) {
 			strcat(position, "|");
 		}
 	}
-	printf("-> Character <%c> appears at position [%s]", c, position);
+	printf("-> Character <%c> appears at position [%s]\n", c, position);
 	return position;
 }
 
@@ -206,12 +242,12 @@ int ex13(char *str, char c) {
 			times++;
 		}
 	}
-	printf("-> Character <%c> appears %d times", c, times);
+	printf("-> Character <%c> appears %d times\n", c, times);
 	return times;
 }
 
 int ex14(char *fullName, char *name) {
-	printf("\n-----EXERCISE 13-----\n");
+	printf("\n-----EXERCISE 14-----\n");
 	printf("Full Name is: <%s>\n", fullName);
 	int i, j, nameLength = strlen(name);
 	for (i = 0, j = 0; fullName[i] != '\0' && name[j] != '\0'; i++) {
@@ -221,89 +257,46 @@ int ex14(char *fullName, char *name) {
             j = 0;
         }
     }
-    if (j == nameLength && (fullName[i] == '\0' || fullName[i] == ' ')) { // Make sure that this is a name! Followed by a space!
-    	printf("-> Found name <%s> inside Full Name !", name);
+    if (j == nameLength && (fullName[i] == '\0' || fullName[i] == ' ')) { // Make sure that this is a name, followed by a space or end !
+    	printf("-> Found name <%s> inside Full Name !\n", name);
         return 1; /* Substring found ! Return TRUE */
     }
-    printf("-> Cannot found <%s> inside Full Name !", name);
+    printf("-> Cannot found <%s> inside Full Name !\n", name);
 	return 0; /* Substring not found ! Return FALSE */
 }
 
-void swapStrings(char** str1, char** str2){
-    char *temp = *str1;
-    *str1 = *str2;
-    *str2 = temp;
-}
-
-char** split(char *s) {
-	int i, index = 0, strlength = strlen(s) + 1;
-	char *str = (char*) calloc(strlength, sizeof(char)); // Initialized $<str> to cut String using strtok() with delimiter.
-	char *word = (char*) calloc(strlength, sizeof(char));
-	char **words = (char**) calloc(strlength, sizeof(char*));
-	
-	for (i = 0; i < strlength; i++) { // Dynamic-continuous allocation for each word in the array of words !
-		words[i] = (char*) calloc(strlength, sizeof(char));
-	}
-	strcpy(str, s); // Copy $<s> to $<str> and print out the copied String.
-	char *p = strtok(str, " ");
-	while (p != NULL) { // Repeat the String-cutting process !
-		strcpy(words[index], p); // Assign the trimmed word $<p> to array of words !
-		p = strtok(NULL, " ");
-		index++;
-	}
-	words[index] = NULL; // Mark that list of words only have N = index (values) !
-	return words;
-}
-
-char** ex15(char *s) {
+char* ex15(char *str) {
 	printf("\n-----EXERCISE 15-----\n");
-	char *str = (char*) calloc(strlen(s) + 1, sizeof(char)); // Initialized $<str> to cut String using strtok() with delimiter.
-	char *word = (char*) calloc(strlen(s) + 1, sizeof(char));
-	char **words = (char**) calloc(strlen(s) + 1, sizeof(char*));
-	int i, index = 0;
-	for (i = 0; i < strlen(s) + 1; i++) { // Dynamic-continuous allocation for each word in the array of words !
-		words[i] = (char*) calloc(strlen(s) + 1, sizeof(char));
-	}
-	printf("The String is: <%s>\n", strcpy(str, s)); // Copy $<s> to $<str> and print out the copied String.
-	char *p = strtok(str, " ");
-	while (p != NULL) { // Repeat the String-cutting process !
-		strcpy(words[index], p); // Assign the cutted word $<p> to array of words !
-		p = strtok(NULL, " ");
-		index++;
-	}
-	// At this point, index == 3, and you have an array of words: words[0] == "I", words[1] == "was", words[2] == "You"
-	swapStrings(words[0], words[index - 1]); // Swap the 1st and the last String !
-	for (i = 0; i < index; i++) {
+	printf("The String is: <%s>\n", str);
+	char *word = getArg(strlen(str));
+	char **words = split(str, " ");
+	int i, index = 0, wlength = atoi(words[0]);
+	swapStrings(words[1], words[wlength]); // Swap the 1st and the last String !
+	for (i = 1; i < wlength; i++) { // Concatenating from the 1st to the word right before the last word !
 		strcat(word, words[i]);
 		strcat(word, " ");
 	}
+	strcat(word, words[i]); // Concatenating the last word !
 	printf("-> The reversed first/last substring is: <%s>\n", word);
-	return words;
+	return word;
 }
 
-char** ex16(char *s) {
+char** ex16(char *str) {
 	printf("\n-----EXERCISE 16-----\n");
-	char *str = (char*) calloc(strlen(s) + 1, sizeof(char)); // Initialized $<str> to cut String using strtok() with delimiter.
-	char *lastName = (char*) calloc(strlen(s) + 1, sizeof(char));
-	char **names = (char**) calloc(strlen(s) + 1, sizeof(char*)); // Storing all the words from the String !
-	int i, index = 0;
-	for (i = 0; i < strlen(s) + 1; i++) { // Dynamic-continuous allocation for each name in the array of names !
-		names[i] = (char*) calloc(strlen(s) + 1, sizeof(char));
-	}
-	printf("The String is: <%s>\n", strcpy(str, s)); // Copy $<s> to $<str> and print out the copied String.
-	char *p = strtok(str, " ");
-	while (p != NULL) { // Repeat the String-cutting process !
-		strcpy(names[index], p); // Assign the cutted word $<p> to array of words !
-		p = strtok(NULL, " ");
-		index++;
-	}
-	// At this point, index == 3, and you have an array of names: names[0] == "Nguyen", names[1] == "Van", names[2] == "A"
-	for (i = 0; i < index - 1; i++) {
-		strcat(lastName, names[i]);
+	printf("The String is: <%s>\n", str);
+	char *lastName = getArg(strlen(str));
+	char **words = split(str, " ");
+	int i, index = 0, wlength = atoi(words[0]);
+	for (i = 1; i < wlength - 1; i++) { // Concatenating from the 1st to the word right before the Last-Name word !
+		strcat(lastName, words[i]);
 		strcat(lastName, " ");
 	}
-	printf("-> Last Name: <%s>\n", lastName);
-	printf("-> First Name: <%s> ", names[index - 1]);
+	strcat(lastName, words[i]); // Concatenating the Last-Name word !
+	char **names = getArgv(2, strlen(str));
+	names[0] = lastName;
+	names[1] = words[wlength];
+	printf("-> Last Name: <%s>\n", names[0]);
+	printf("-> First Name: <%s>\n", names[1]);
 	return names;
 }
 
@@ -313,11 +306,11 @@ int ex17(char *str) {
 	int l = 0, h = strlen(str) - 1;	// Start from leftmost and rightmost corners of <$str>
 	while (h > l) {	// Keep comparing characters, using 2 pointers until they cross-over each others.
 		if (tolower(str[l++]) != tolower(str[h--])) { // Lowercasing the letter before checking !
-			printf("-> <%s> is Not Palindrome", str);
+			printf("-> <%s> is Not Palindrome\n", str);
 			return 0; // Return FALSE
 		}
 	}
-	printf("-> <%s> is Palindrome", str);
+	printf("-> <%s> is Palindrome\n", str);
 	return 1; // Return TRUE
 }
 
@@ -330,15 +323,16 @@ int* ex18(char *str) {
 	for (i = 0; i < strlen(str); i++) { // Print out the value of each extracted digits !
 		if (str[i] > '0' && str[i] < '9') {
 			printf("[%c]", str[i]);
-			digitArray[j++] = str[i] - '0';
+			digitArray[j++] = str[i] - '0'; // Convert character to integer !
 		}
 	}
+	printf("\n");
 	return digitArray; // Return an array of extracted digits.
 }
 
 char* ex19(char *s, char delete) {
-	printf("\n-----EXERCISE 5-----\n");
-	char *str = (char*) calloc(strlen(s) + 1, sizeof(char));
+	printf("\n-----EXERCISE 19-----\n");
+	char *str = getArg(strlen(s) + 1);
 	printf("The String is: <%s>\n", strcpy(str, s));
 	int i = 0, j = 0;
 	while (str[i]) {
@@ -348,7 +342,7 @@ char* ex19(char *s, char delete) {
 		i++;
 	}
 	str[j] = '\0';
-	printf("-> String after deleting <%c> = <%s>", delete, str);
+	printf("-> String after deleting <%c> = <%s>\n", delete, str);
 	return str;
 }
 
@@ -356,7 +350,7 @@ char* ex20(char *str) {
 	printf("\n-----EXERCISE 20-----\n");
 	printf("The String is: <%s>\n", str);
 	int *visited = (int*) calloc(500, sizeof(int)); // 500 continous data blocks to count visited characters !
-	char *charsMaxFreq = (char*) calloc(strlen(str) + 1, sizeof(char)); // String contains max-frequency characters !
+	char *charsMaxFreq = getArg(strlen(str) + 1); // String contains max-frequency characters !
 	int i, j = 0, maxFrequency = 0;
 	for (i = 0; i < strlen(str); i++) {
 		if (maxFrequency < ++visited[str[i]]) {
@@ -370,13 +364,13 @@ char* ex20(char *str) {
 		charsMaxFreq[j] = '\0';
 	}
 	printf("-> Characters with Max Frequency: <%s>\n", charsMaxFreq);
-	printf("-> Number of occurrences: %d ", maxFrequency);
+	printf("-> Number of occurrences: %d\n", maxFrequency);
 	return charsMaxFreq;
 }
 
 char* ex21(char *s, char *delete) {
 	printf("\n-----EXERCISE 21-----\n");
-	char *str = (char*) calloc(strlen(s) + 1, sizeof(char));
+	char *str = getArg(strlen(s) + 1);
 	printf("The String is: <%s>\n", strcpy(str, s));
 	int i, j, start, stop, deleteLength = strlen(delete);
 	
@@ -398,7 +392,7 @@ char* ex21(char *s, char *delete) {
     	str[i] = str[i + deleteLength];
 	}
 	str[i] = '\0';
-	printf("-> String after deleting <%s> = <%s>", delete, str);
+	printf("-> String after deleting <%s> = <%s>\n", delete, str);
 	return str;
 }
 
